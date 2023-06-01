bb99d2474e7a3656211962a0c7dcf67d1e86c7ed
Patrick Nelson <patnelson@ebay.com>
2023-06-01 13:51:11 -0700

    Including example of default slot affected by same issue.

---
+++ src/lib/ExampleNoShadowDefault.svelte
@@ -0,0 +1,16 @@
+<svelte:options
+	customElement={{
+		tag: 'example-no-shadow-default',
+		shadow: 'none',
+	}}
+/>
+
+<h2>shadow: 'none' (default slot)</h2>
+<p>Default slot:</p>
+<slot><p>Fallback content 1</p></slot>
+
+<style>
+	p {
+		background-color: azure;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import ExampleShadow from './lib/ExampleShadow.svelte';
 import ExampleNoShadow from './lib/ExampleNoShadow.svelte';
+import ExampleNoShadowDefault from './lib/ExampleNoShadowDefault.svelte';
 
 export {
 	ExampleShadow,
 	ExampleNoShadow,
+	ExampleNoShadowDefault,
 };
